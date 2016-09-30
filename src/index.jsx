03e6b16582452f511de8486f276bdb0a534a236b
Ole <ole@vprsm.de>
2016-09-30 15:14:06 +0200

    Add rune slot display

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,51 @@
+"use strict";
+
 import React, {Component} from "react";
 import ReactDOM from "react-dom"
+import Runes from "./runes.jsx";
+
+class Slot extends Component {
+	render() {
+		if (this.props.runeID != null && this.props.runeID in Runes) {
+			const rune = Runes[this.props.runeID];
+
+			return (
+				<div className="slot">
+					<div className="holder">
+						<div className={`rune ${rune.type}`}></div>
+					</div>
+					<div className="description">
+						<div className="name">
+							{rune.statName}
+						</div>
+						<div className="set-name">
+							+{rune.power} {rune.setName}
+						</div>
+					</div>
+					<div className="rating">
+						{rune.statValue}
+					</div>
+				</div>
+			);
+		} else {
+			return (
+				<div className="slot">
+					<div className="holder"></div>
+				</div>
+			);
+		}
+	}
+}
 
 class SiteRoot extends Component {
 	render() {
-		return <div>Hello World</div>;
+		return (
+			<div>
+				<Slot runeID={0} />
+				<Slot runeID={1} />
+				<Slot />
+			</div>
+		);
 	}
 }
 
---
+++ src/runes.jsx
@@ -0,0 +1,35 @@
+"use strict";
+
+const runes = [
+	{
+		type: "earth",
+		setName: "Voice From Beyond",
+		statName: "Multi-Hit Severity Rating",
+		statValue: 501,
+		power: 2,
+		unique: true
+	},
+	{
+		type: "life",
+		setName: "Voice From Beyond",
+		statName: "Focus Pool",
+		statValue: 75,
+		power: 1
+	},
+	{
+		type: "air",
+		setName: "Voice From Beyond",
+		statName: "Focus Recovery Rating",
+		statValue: 501,
+		power: 1
+	},
+	{
+		type: "life",
+		setName: "Voice From Beyond",
+		statName: "Intensity Rating",
+		statValue: 501,
+		power: 1
+	}
+];
+
+export default runes;
