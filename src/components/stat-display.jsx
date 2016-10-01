5ec6e2690d4fd5e3a9b7bc6b77fb7f9d78bb916f
Ole <ole@vprsm.de>
2016-10-01 17:27:16 +0200

    Rename 'LoadoutSummary' to 'StatOverview'

---
+++ src/components/stat-display.jsx
@@ -0,0 +1,32 @@
+"use strict";
+
+import React, {Component} from "react";
+import Runes from "../database/runes.jsx";
+import Stats from "../database/stats.jsx";
+
+export default class StatDisplay extends Component {
+	render() {
+		let stats = {};
+
+		if (this.props.runes)
+			stats = Stats.gatherStats(this.props.runes);
+		else if (this.props.stats)
+			stats = this.props.stats;
+
+		let counter = 0;
+		const lines = Object.keys(stats).sort().map(name => {
+			return (
+				<div key={counter++} className="property">
+					<div className="name">
+						{name}
+					</div>
+					<div className="value">
+						{Stats.formatStat(name, stats[name])}
+					</div>
+				</div>
+			);
+		});
+
+		return <div className="summary">{lines}</div>;
+	}
+}
